13b137161c467e995f9db671f9e5ace16e2678ad
StealthDemon <kubuskos@gmail.com>
2022-04-04 16:37:50 +0200

    Add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SequentialSearchComponent } from './sequential-search/sequential-search.component';
-import { HashSearchComponent } from './hash-search/hash-search.component';
-import { BinaryTreeSearchComponent } from './binary-tree-search/binary-tree-search.component';
 import { KnuthSearchComponent } from './knuth-search/knuth-search.component';
 import { BoyerSearchComponent } from './boyer-search/boyer-search.component';
 import { RabinSearchComponent } from './rabin-search/rabin-search.component';
+import { BinaryTreeSearchComponent } from './binary-tree-search/binary-tree-search.component';
+import { HashSearchComponent } from './hash-search/hash-search.component';
 
 @NgModule({
   declarations: [
