90048f30d7918de8bfaa1adf07bc940c7d9851b5
Maxym <smolinskimaksym@gmail.com>
2022-04-26 12:39:40 +0200

    Added hashsearch script

--- js/search/sequentialsearch/hashSearch.js
+++ js/search/sequentialsearch/hashSearch.js
@@ -1,40 +1,40 @@
-function hash(s){
-    let h = 0;
-    for(i=0; i<s.length; i++) h = 13*i*h^(s.charCodeAt(i)%65);
-    return Math.abs(h%10000);
+function hash(s) {
+  let h = 0;
+  for (i = 0; i < s.length; i++) h = (13 * i * h) ^ s.charCodeAt(i) % 65;
+  return Math.abs(h % 10000);
 }
 
-function hashTable(data){
-    let result = new Array();
-    for(const i in data){
-        for(const j in data[i].keys){
-            const h = hash(data[i].keys[j]);
-            if(!result[h]) result[h] = [];
-            const record = {}
-            record.key = data[i].keys[j];
-            record.value = data[i].value;
-            result[h].push(record);
-        }
+function hashTable(data) {
+  let result = new Array();
+  for (const i in data) {
+    for (const j in data[i].keys) {
+      const h = hash(data[i].keys[j]);
+      if (!result[h]) result[h] = [];
+      const record = {};
+      record.key = data[i].keys[j];
+      record.value = data[i].value;
+      result[h].push(record);
     }
-    return result;
+  }
+  return result;
 }
 
-function hashSearch(query, data, hashTime){
-    const result = {};
-    result.result = [];
-    const start = new Date().getTime();
-    const h = hash(query);
+function hashSearch(query, data, hashTime) {
+  const result = {};
+  result.result = [];
+  const start = new Date().getTime();
+  const h = hash(query);
 
-    for(const i in data[h]){
-        if(data[h][i].key == query) result.result.push(data[h][i].value);
-    }
+  for (const i in data[h]) {
+    if (data[h][i].key == query) result.result.push(data[h][i].value);
+  }
 
-    if(!result.result.length) result.error = "nie znaleziono żadnych wyników";
-    result.query = query;
-    result.hashTime = hashTime;
-    result.time = new Date().getTime() - start;
+  if (!result.result.length) result.error = "nie znaleziono żadnych wyników";
+  result.query = query;
+  result.hashTime = hashTime;
+  result.time = new Date().getTime() - start;
 
-    return result;
+  return result;
 }
 
 module.exports.hashTable = hashTable;
--- js/search/sequentialsearch/server.js
+++ js/search/sequentialsearch/server.js
@@ -1,25 +1,28 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
-const {port} = require('./config.json');
-let {data} = require('./data.json');
-const {hashTable, search} = require('./hashSearch');
+const { port } = require("./config.json");
+let { data } = require("./data.json");
+const { hashTable, search } = require("./hashSearch");
 
 const start = new Date().getTime();
 data = hashTable(data);
 const hasTime = new Date().getTime() - start;
 
 const server = http.createServer((req, res) => {
-    let query = url.parse(req.url, true).query.q;
-    
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Content-Type', 'application/json');
-    
-    if(query)res.end(JSON.stringify(search(query, data, hasTime)));
-    else res.end('{"error":"nie podano zapytania (paramet q wysłany metodą get)"}')
+  let query = url.parse(req.url, true).query.q;
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Content-Type", "application/json");
+
+  if (query) res.end(JSON.stringify(search(query, data, hasTime)));
+  else
+    res.end('{"error":"nie podano zapytania (paramet q wysłany metodą get)"}');
 });
-  
-server.listen(process.env.PORT || port, () => {console.log(`listen port ${process.env.PORT || port}`);});
+
+server.listen(process.env.PORT || port, () => {
+  console.log(`listen port ${process.env.PORT || port}`);
+});
