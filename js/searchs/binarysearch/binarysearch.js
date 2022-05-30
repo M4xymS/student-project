1485db0884768f9d61a2326fa321459079f16de7
Maxym <smolinskimaksym@gmail.com>
2022-05-30 20:35:07 +0200

    added binary and kmp search

---
+++ js/searchs/binarysearch/binarysearch.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", funkcjabinary);
+function funkcjabinary() {
+  const query = document.querySelector("#q").value;
+
+  let url = `http://[::1]:3000?q=${query}`;
+  url = `https://binary-search.herokuapp.com/?q=${query}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      if (res.result == 0)
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Brak wyników!",
+        });
+      else
+        for (const el of res.result)
+          Swal.fire({
+            icon: `success`,
+            title: `Sukces, wyniki zapytania ${query} to:`,
+            text: `${res.result} `,
+          });
+    });
+}
